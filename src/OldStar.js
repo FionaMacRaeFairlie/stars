428023b63192cbe4aa986bc989db2e8013206e2b
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2021-10-13 17:04:05 +0100

    Delete OldStar.js

--- src/OldStar.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { FaStar } from "react-icons/fa";
-
-export default function OldStar({ selected = false, onClick = f => f }) {
-  return <FaStar color={selected ? "red" : "grey"} onClick={onClick} />;
-}
